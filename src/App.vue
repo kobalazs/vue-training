022e0457a7c427c843f3e833d135bf6e88adf6b8
Balázs Kovács <balazs.kovacs@stylersonline.com>
2020-06-08 14:38:41 +0200

    Reorganize navigation & clear up the home page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <NavigationComponent />
+    <router-view />
   </div>
 </template>
+
+<script>
+import NavigationComponent from './components/NavigationComponent.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NavigationComponent
+  }
+};
+</script>
 
 <style>
 #app {
