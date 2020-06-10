160db2bf53363cdb5de6f588654a241ce3859f77
Balázs Kovács <balazs.kovacs@stylersonline.com>
2020-06-10 23:51:51 +0200

    Create CalculatorComponent using options-based API

---
+++ src/components/CalculatorComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>Calculator</h2>
+    <b-button @click="increment" class="d-block mb-2">
+      Count is: {{ count }}, double is: {{ double }}
+    </b-button>
+    <b-button @click="fibonacci">
+      First is: {{ first }}, second is: {{ second }}, sum is: {{ sum }}
+    </b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    count: 0,
+    first: 0,
+    second: 1
+  }),
+  computed: {
+    double() {
+      return this.count * 2
+    },
+    sum() {
+      return this.first + this.second
+    }
+  },
+  methods: {
+    increment() {
+      this.count += 1
+    },
+    fibonacci() {
+      const temp = this.first + this.second
+      this.first = this.second
+      this.second = temp
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,5 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" class="float-right">
     <h1>Welcome to vue-training!</h1>
+    <CalculatorComponent />
   </div>
 </template>
+
+<script>
+import CalculatorComponent from '../components/CalculatorComponent'
+
+export default {
+  components: {
+    CalculatorComponent
+  }
+}
+</script>
