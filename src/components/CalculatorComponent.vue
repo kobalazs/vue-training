536d5a8e540a68a2b7fd98ed0dccc3607c030f49
Balázs Kovács <balazs.kovacs@stylersonline.com>
2020-06-11 23:07:56 +0200

    Separate logical concerns of CalculatorComponent

--- src/components/CalculatorComponent.vue
+++ src/components/CalculatorComponent.vue
@@ -2,10 +2,10 @@
   <div>
     <h2>Calculator</h2>
     <b-button @click="increment" class="d-block mb-2">
-      Count is: {{ state.count }}, double is: {{ state.double }}
+      Count is: {{ incrementState.count }}, double is: {{ incrementState.double }}
     </b-button>
     <b-button @click="fibonacci">
-      First is: {{ state.first }}, second is: {{ state.second }}, sum is: {{ state.sum }}
+      First is: {{ fibonacciState.first }}, second is: {{ fibonacciState.second }}, sum is: {{ fibonacciState.sum }}
     </b-button>
   </div>
 </template>
@@ -15,29 +15,45 @@ import { reactive, computed } from '@vue/composition-api'
 
 export default {
   setup() {
-    const state = reactive({
-      count: 0,
-      first: 0,
-      second: 1,
-      double: computed(() => state.count * 2),
-      sum: computed(() => state.first + state.second)
-    })
-
-    function increment() {
-      state.count += 1
-    }
-
-    function fibonacci() {
-      const temp = state.first + state.second
-      state.first = state.second
-      state.second = temp
-    }
-
     return {
-      state,
-      increment,
-      fibonacci
+      ...useIncrementCalculator(),
+      ...useFibonacciCalculator()
     }
+  }
+}
+
+function useIncrementCalculator() {
+  const incrementState = reactive({
+    count: 0,
+    double: computed(() => incrementState.count * 2)
+  })
+
+  function increment() {
+    incrementState.count += 1
+  }
+
+  return {
+    incrementState,
+    increment
+  }
+}
+
+function useFibonacciCalculator() {
+  const fibonacciState = reactive({
+    first: 0,
+    second: 1,
+    sum: computed(() => fibonacciState.first + fibonacciState.second)
+  })
+
+  function fibonacci() {
+    const temp = fibonacciState.first + fibonacciState.second
+    fibonacciState.first = fibonacciState.second
+    fibonacciState.second = temp
+  }
+
+  return {
+    fibonacciState,
+    fibonacci
   }
 }
 </script>
